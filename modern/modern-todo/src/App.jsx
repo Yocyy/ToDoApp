6b0b0879383d75d19a97eebf19855917efa666a4
Yocyy <yocy0522@gmail.com>
2021-12-14 00:12:12 +0900

    追加機能

--- modern/modern-todo/src/App.jsx
+++ modern/modern-todo/src/App.jsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import "./styles.css"
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setincompleteTodos] = useState(['test1','test2']);
   const [completeTodos, setcompleteTodos] = useState(['aa','bb']);
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if(!todoText) return;
+    const newTodos = [...incompleteTodos, todoText];
+    setincompleteTodos(newTodos);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
